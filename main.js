5a123d8e316bc7bd1b8ae8ecc48951c11d8230f2
Andrei Alexandrov <andrei.alxv@gmail.com>
2024-02-20 00:15:51 +0200

    exploring menu items

--- main.js
+++ main.js
@@ -1,5 +1,55 @@
-const { app, BrowserWindow } = require("electron");
+const { app, shell, BrowserWindow, Menu } = require("electron");
 const path = require("node:path");
+const isMac = process.platform === "darwin";
+
+//This is the menu template
+const menuItems = [
+  {
+    label: "File",
+    submenu: [
+      {
+        label: "Learn more",
+        click: async () => {
+          await shell.openExternal(
+            "https://andrei-alexandrov-portfolio.netlify.app/"
+          );
+        },
+      },
+      {
+        type: "separator",
+      },
+      isMac ? { role: "close" } : { role: "quit" },
+      // {
+      //   label: "Exit",
+      //   click: () => {
+      //     app.quit();
+      //   },
+      // },
+    ],
+  },
+  {
+    label: "Menu",
+    submenu: [
+      {
+        label: "About",
+      },
+    ],
+  },
+  {
+    label: "Window",
+    submenu: [
+      {
+        role: "close",
+      },
+      {
+        role: "minimize",
+      },
+    ],
+  },
+];
+
+const menu = Menu.buildFromTemplate(menuItems);
+Menu.setApplicationMenu(menu);
 
 const createWindow = () => {
   const window = new BrowserWindow({
